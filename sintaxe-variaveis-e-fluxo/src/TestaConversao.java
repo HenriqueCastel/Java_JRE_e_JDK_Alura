ca7f4464b5f3327e7547b426ef6496709d89ba1a
Henrique Haas Dal Castel <77808747+HenriqueCastel@users.noreply.github.com>
2022-09-21 11:34:30 -0300

    Add files via upload

---
+++ sintaxe-variaveis-e-fluxo/src/TestaConversao.java
@@ -0,0 +1,20 @@
+
+public class TestaConversao {
+	
+	public static void main(String[] args) {
+		double salario = 1270.50;
+		
+		float pontoFlutuante = 3.14f;
+		
+		int valor = (int) salario;
+		System.out.println(valor);
+		
+		long numeroGrande = 32432423523L;
+		
+		double valor1 = 0.2;
+		double valor2 = 0.1;
+		double total = valor1 + valor2;
+		
+		System.out.println(total);	
+	}
+}
